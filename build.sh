c596ddee75ae81ce50d8b669657ccd541bf601e4
Ahmad Zendebudi <ahmadzendebudi@gmail.com>
2023-04-13 06:59:07 +0000

    added kubectl command to apply changes to the cluster

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ docker build -t zendebudi/travel-planner-frontend ./frontend/
 docker push zendebudi/travel-planner-backend
 docker push zendebudi/travel-planner-frontend
 
-kubectl apply -f ./kubernetes/backend -f ./kubernetes/frontend -f ./kubernetes/adminer
+kubectl apply -f kubernetes/backend.yaml -f kubernetes/frontend.yaml -f kubernetes/adminer.yaml
 
 exit 0
